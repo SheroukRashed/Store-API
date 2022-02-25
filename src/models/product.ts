e501a2edd476d4565fbfb2c9ded8480fb50aba32
Sherouk Rashed <sherouk.mohammed.rashed@gmail.com>
2022-02-25 13:34:15 +0200

    Finish most basic queries

---
+++ src/models/product.ts
@@ -0,0 +1,77 @@
+// @ts-ignore
+import client from '../database'
+
+export type Product = {
+  id: number
+  name: string
+  price: number
+  category_id: number
+}
+
+export class ProductModel {
+  static async index(): Promise<Product[]> {
+    try {
+      const sql = 'SELECT * FROM products'
+      // @ts-ignore
+      const conn = await client.connect()
+
+      const result = await conn.query(sql)
+
+      conn.release()
+
+      return result.rows
+    } catch (err) {
+      throw new Error(`Could not get products. Error: ${err}`)
+    }
+  }
+
+  static async show(id: string): Promise<Product> {
+    try {
+      const sql = 'SELECT * FROM products WHERE id=($1)'
+      // @ts-ignore
+      const conn = await client.connect()
+
+      const result = await conn.query(sql, [id])
+
+      conn.release()
+
+      return result.rows[0]
+    } catch (err) {
+      throw new Error(`Could not find product ${id}. Error: ${err}`)
+    }
+  }
+
+  static async create(product: Product): Promise<Product> {
+    try {
+      const sql = 'INSERT INTO books (name, price, category_id) VALUES($1, $2, $3) RETURNING *'
+      // @ts-ignore
+      const conn = await client.connect()
+
+      const result = await conn.query(sql, [product.name, product.price, product.category_id])
+
+      const createdProduct = result.rows[0]
+
+      conn.release()
+
+      return createdProduct
+    } catch (err) {
+      throw new Error(`Could not add new product ${product.name}. Error: ${err}`)
+    }
+  }
+
+  static async showByCategory(categoryId: string): Promise<Product[]> {
+    try {
+      const sql = 'SELECT * FROM products WHERE category_id=($1)'
+      // @ts-ignore
+      const conn = await client.connect()
+
+      const result = await conn.query(sql, [categoryId])
+
+      conn.release()
+
+      return result.rows
+    } catch (err) {
+      throw new Error(`Could not find any products in category ${categoryId}. Error: ${err}`)
+    }
+  }
+}
---
+++ src/models/user.ts
@@ -0,0 +1,62 @@
+// @ts-ignore
+import client from '../database'
+
+export type User = {
+  id: number
+  firstName: string
+  lastName: string
+  passwordDigest: string
+}
+
+export class UserModel {
+  static async index(): Promise<User[]> {
+    try {
+      const sql = 'SELECT * FROM users'
+      // @ts-ignore
+      const conn = await client.connect()
+
+      const result = await conn.query(sql)
+
+      conn.release()
+
+      return result.rows
+    } catch (err) {
+      throw new Error(`Could not get users. Error: ${err}`)
+    }
+  }
+
+  static async show(id: string): Promise<User> {
+    try {
+      const sql = 'SELECT * FROM users WHERE id=($1)'
+      // @ts-ignore
+      const conn = await client.connect()
+
+      const result = await conn.query(sql, [id])
+
+      conn.release()
+
+      return result.rows[0]
+    } catch (err) {
+      throw new Error(`Could not find user ${id}. Error: ${err}`)
+    }
+  }
+
+  static async create(user: User): Promise<User> {
+    try {
+      const sql =
+        'INSERT INTO users (first_name, last_name, password_digest) VALUES($1, $2, $3) RETURNING *'
+      // @ts-ignore
+      const conn = await client.connect()
+
+      const result = await conn.query(sql, [user.firstName, user.lastName, user.passwordDigest])
+
+      const createdUser = result.rows[0]
+
+      conn.release()
+
+      return createdUser
+    } catch (err) {
+      throw new Error(`Could not add new user ${user.firstName} ${user.lastName} Error: ${err}`)
+    }
+  }
+}
